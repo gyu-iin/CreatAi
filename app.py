08111a59424744a11b42956da225f78094f2d319
gyu-iin <kimgm9907@gmail.com>
2024-11-12 15:24:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ messages = [
 ]
 
 
-query = st.text_input('무엇을 묻고싶나요? / 종료하고싶다면 exit')
+query = st.text_input('무엇을 묻고싶나요?')
 messages.append({"role": "user", "content": query})
 response = client.chat.completions.create(
   model = "gpt-4o-mini",
@@ -20,3 +20,10 @@ response = client.chat.completions.create(
 st.write(f"{response.choices[0].message.content}")
 st.write("")
 messages.append({"role": "assistant", "content": response.choices[0].message.content})
+
+img = st.text_input('생성하고싶은 이미지를 입력하세요')
+response = client.images.generate(
+model="dall-e-3",
+prompt=img)
+image_url = response.data[0].url
+st.markdown("![alt text](image_url)")
