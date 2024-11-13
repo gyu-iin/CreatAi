8f17a5ce8f03f0e909420c7331bae3bc24a1f1ed
gyu-iin <kimgm9907@gmail.com>
2024-11-13 14:32:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,24 +6,18 @@ st.write("API KEY")
 key = st.text_input('API KEY를 작성하세요', type="password")
 client = OpenAI(api_key=f"{key}")
 
+prompt = st.text_area("Prompt")
+
 messages = [
-    {"role": "system", "content": "너는 질문에 대답하는 AI야."}
+    {"role": "user", "content": prompt}
 ]
 
+answer = ''
 
-# query = st.text_input('무엇을 묻고싶나요?')
-# messages.append({"role": "user", "content": query})
-# response = client.chat.completions.create(
-#   model = "gpt-4o-mini",
-#   messages = messages
-# )
-# st.write(f"{response.choices[0].message.content}")
-# st.write("")
-# messages.append({"role": "assistant", "content": response.choices[0].message.content})
-
-img = st.text_input('생성하고싶은 이미지를 입력하세요')
-response = client.images.generate(
-    model="dall-e-3",
-    prompt=img)
-image_url = response.data[0].url
-st.image(image_url)
+if st.button("Generate"):
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages
+    )
+    answer = response.choices[0].message.content
+st.text(answer)
