0b259946e535f21863293aeea59479872c9e324f
gyu-iin <kimgm9907@gmail.com>
2024-11-13 15:19:39 +0900

    Update 1_Img.py

--- pages/1_Img.py
+++ pages/1_Img.py
@@ -1,1 +1,11 @@
+prompt = st.session_state.prompt
+response = client.images.generate(
+    model="dall-e-3",
+    prompt=prompt,
+    n=1,
+    size="1024x1024"
+)
+image_url = response.data[0].url
 
+if image_url:
+    st.markdown(f"![{prompt}]({image_url})")
