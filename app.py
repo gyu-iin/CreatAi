c73fd4bc73305d47b09da85b0d371832199dcfc9
gyu-iin <kimgm9907@gmail.com>
2024-11-13 14:54:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import streamlit as st
 from openai import OpenAI
 
+@st.cache_data
+def ask_answer(prompt):
+    messages = [
+        {"role": "user", "content": prompt}
+    ]
+    
+    answer = ''
+    
+    if st.button("Generate"):
+        response = client.chat.completions.create(
+            model = "gpt-4o-mini",
+            messages = messages
+        )
+        answer = response.choices[0].message.content
+    return answer
+
 st.title("GPT-4O mini 사용")
 st.write("API KEY")
-key = st.text_input('API KEY를 작성하세요', type="password")
-client = OpenAI(api_key=f"{key}")
+Apikey = st.text_input('API KEY를 작성하세요', type="password")
+st.session_state.Apikey
+client = OpenAI(api_key=f"{Apikey}")
 
 prompt = st.text_area("Prompt")
-
-messages = [
-    {"role": "user", "content": prompt}
-]
-
-answer = ''
-
-if st.button("Generate"):
-    response = client.chat.completions.create(
-        model = "gpt-4o-mini",
-        messages = messages
-    )
-    answer = response.choices[0].message.content
-st.text(answer)
+st.text(ask_answer(prompt))
